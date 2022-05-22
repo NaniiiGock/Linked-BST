b9bf163b24eb8a67c615a59ce52980c34857a271
NaniiiGock <91615473+NaniiiGock@users.noreply.github.com>
2022-05-22 23:34:48 +0300

    Update linkedqueue.py

--- linkedqueue.py
+++ linkedqueue.py
@@ -1,82 +1,67 @@
-# Copyright 2013, Michael H. Goldwasser
-#
-# Developed for use with the book:
-#
-#    Data Structures and Algorithms in Python
-#    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
-#    John Wiley & Sons, 2013
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+File: linkedqueue.py
+Author: Ken Lambert
+"""
 
-from exceptions import Empty
+from node import Node
+from abstractcollection import AbstractCollection
 
-class LinkedQueue:
-  """FIFO queue implementation using a singly linked list for storage."""
+class LinkedQueue(AbstractCollection):
+    """A link-based queue implementation."""
 
-  #-------------------------- nested _Node class --------------------------
-  class _Node:
-    """Lightweight, nonpublic class for storing a singly linked node."""
-    __slots__ = '_element', '_next'         # streamline memory usage
+    # Constructor
+    def __init__(self, sourceCollection = None):
+        """Sets the initial state of self, which includes the
+        contents of sourceCollection, if it's present."""
+        self._front = self._rear = None
+        AbstractCollection.__init__(self, sourceCollection)
 
-    def __init__(self, element, next):
-      self._element = element
-      self._next = next
+    # Accessor methods
+    def __iter__(self):
+        """Supports iteration over a view of self."""
+        cursor = self._front
+        while not cursor is None:
+            yield cursor.data
+            cursor = cursor.next
+        
+    def peek(self):
+        """
+        Returns the item at the front of the queue.
+        Precondition: the queue is not empty.
+        Raises: KeyError if the stack is empty."""
+        if self.isEmpty():
+            raise KeyError("The queue is empty.")
+        return self._front.data
 
-  #------------------------------- queue methods -------------------------------
-  def __init__(self):
-    """Create an empty queue."""
-    self._head = None
-    self._tail = None
-    self._size = 0                          # number of queue elements
+    # Mutator methods
+    def clear(self):
+        """Makes self become empty."""
+        self._size = 0
+        self._front = self._rear = None
 
-  def __len__(self):
-    """Return the number of elements in the queue."""
-    return self._size
+    def add(self, item):
+        """Adds item to the rear of the queue."""
+        newNode = Node(item, None)
+        if self.isEmpty():
+            self._front = newNode
+        else:
+            self._rear.next = newNode
+        self._rear = newNode
+        self._size += 1
 
-  def is_empty(self):
-    """Return True if the queue is empty."""
-    return self._size == 0
-
-  def first(self):
-    """Return (but do not remove) the element at the front of the queue.
-
-    Raise Empty exception if the queue is empty.
-    """
-    if self.is_empty():
-      raise Empty('Queue is empty')
-    return self._head._element              # front aligned with head of list
-
-  def dequeue(self):
-    """Remove and return the first element of the queue (i.e., FIFO).
-
-    Raise Empty exception if the queue is empty.
-    """
-    if self.is_empty():
-      raise Empty('Queue is empty')
-    answer = self._head._element
-    self._head = self._head._next
-    self._size -= 1
-    if self.is_empty():                     # special case as queue is empty
-      self._tail = None                     # removed head had been the tail
-    return answer
-
-  def enqueue(self, e):
-    """Add an element to the back of queue."""
-    newest = self._Node(e, None)            # node will be new tail node
-    if self.is_empty():
-      self._head = newest                   # special case: previously empty
-    else:
-      self._tail._next = newest
-    self._tail = newest                     # update reference to tail node
-    self._size += 1
+    def pop(self):
+        """
+        Removes and returns the item at the front of the queue.
+        Precondition: the queue is not empty.
+        Raises: KeyError if the queue is empty.
+        Postcondition: the front item is removed from the queue."""
+        if self.isEmpty():
+            raise KeyError("The queue is empty.")
+        oldItem = self._front.data
+        self._front = self._front.next
+        if self._front is None:
+            self._rear = None
+        self._size -= 1
+        return oldItem
+        
+        
